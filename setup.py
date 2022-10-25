dee4430cee9f304d453a55e54bafa07819dde744
Biwwe <baziz@planview.com>
2022-10-25 12:15:32 +0200

    Remove py as dependency, update pytest version in tox.ini
    We won't go out of our way to support python 2 anymore

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(name='pytest-tags',
       packages=['pytest_tags'],
       entry_points={'pytest11': ['tags = pytest_tags.pytest_tags']},
       setup_requires=['setuptools_scm'],
-      install_requires=['pytest>=2.9.0'],
+      install_requires=['pytest>=3.7.2'],
       license='Mozilla Public License 2.0 (MPL 2.0)',
-      keywords='py.test pytest tags',
+      keywords='pytest pytest tags',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Framework :: Pytest',
@@ -37,5 +37,5 @@ setup(name='pytest-tags',
           'Topic :: Software Development :: Testing',
           'Topic :: Utilities',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.6'])
+          'Programming Language :: Python :: 3.6'
+      ])
